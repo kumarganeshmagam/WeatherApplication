9ab1f6951c9c437d14f0fcdfdadb8278801d7a40
Kumar Ganesh Magam <76594791+kumarganeshmagam@users.noreply.github.com>
2024-08-31 17:07:02 +0530

    Update script.js with all functionalities

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ let currentArea="";
 let currentLatitude;
 let currentLongitude;
 let areaList = {};
+let filterValue = 'hourly';
 let currentName="";
+let currentData = '';
 let baseUrl = "https://api.open-meteo.com/v1/forecast?"
 let params = "&current=relative_humidity_2m,is_day,rain,temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+let daywisedata = "&past_days=15";
+class Area {
+    // Define methods for Area class
+    async create(currentName, currentLatitude, currentLongitude) {
+        // Example method implementation
+        return {
+            name: currentName,
+            latitude: currentLatitude,
+            longitude: currentLongitude
+        };
+    }
+}
 
+/* On Load Script Executions START */
+
+//onLoad DataLoad
 renderOnLoad();
+
+//Event Handler for Search
 document.getElementById('search-input').addEventListener('input', async function() {
     const query = this.value;
 
@@ -41,6 +60,7 @@ document.getElementById('search-input').addEventListener('input', async function
     }
 });
 
+//Event Handler for clicking on suggestions
 document.getElementById('suggestions').addEventListener('click', async function(e) {
     if (e.target.dataset.lat && e.target.dataset.lon) {
         const lat = e.target.getAttribute('data-lat');
@@ -53,13 +73,13 @@ document.getElementById('suggestions').addEventListener('click', async function(
         let url = baseUrl + "latitude=" + currentLatitude + "&longitude=" + currentLongitude + params;
 
         try {
-            let data = await getResponse(url);
-            let name = await getName(currentLatitude, currentLongitude);
+            currentData = await getResponse(url);
+            currentName = await getName(currentLatitude, currentLongitude);
 
-            console.log(name);
-            await populateData(data, name);
-
-            console.log(currentLatitude);
+            console.log(currentName);
+            await populateData();
+            await refreshDropdown();
+            console.log("Loaded Data");
         } catch (error) {
             console.error('Error handling click event:', error);
         }
@@ -74,25 +94,88 @@ document.getElementById('suggestions').addEventListener('click', async function(
     }
 });
 
+//Event Handler to click on pin
+document.getElementById('pin').addEventListener('click', async function(){
+    alert("Initiating to ping the currentArea");
+    await PinOrUnpinDropDown("pin")
+});
+document.getElementById('unpin').addEventListener('click',async function(){
+    await PinOrUnpinDropDown("unpin")
+});
+
+
+const dropdown = document.getElementById('location');
+dropdown.addEventListener('change', async function(event){
+
+    const selectedValue = event.target.value;
+    const selectedText = event.target.options[event.target.selectedIndex].text;
+    console.log('Selected value:', selectedValue);
+    console.log('Selected text:', selectedText);
+    if(pinnedAreas.includes(selectedValue)){
+        currentName = selectedValue;
+        let area = areaList[currentName];
+        let url = baseUrl + "latitude=" + area.latitude + "&longitude=" + area.longitude + params;
+        currentData = await getResponse(url);
+        await populateData();
+    }else{
+        alert("please select valid pinnned option using " + currentName);
+    }
+    await refreshDropdown();
+});
+
+
+
+
+//Code to event handle click on radio buttons
+let radios = document.getElementsByName('option');
+// Add click event listener to each radio button
+radios.forEach(async function(radio) {
+    radio.addEventListener('click', async function(event) {
+        if(event.target.value === 'hourly'){
+            if(!currentData.hourly) {
+                let url = baseUrl + "latitude=" + currentLatitude + "&longitude=" + currentLongitude + params;
+                currentData = await getResponse(url);
+                // Right side page
+                await displayTable();
+            }
+            alert("Data Updated to hourly");
+        }
+        else if(event.target.value === 'day-wise'){
+            if(!currentData.hourly){
+                let url = baseUrl + "latitude=" + currentLatitude + "&longitude=" + currentLongitude + daywisedata;
+                currentData = await getResponse(url);
+                // Right side page
+                await displayTable();
+            }
+            alert("Data Updated to day-wise");
+        }
+    });
+});
+/* On Load Script Executions END */
 
 async function renderOnLoad(){
     console.log("loading..");
     getLocation();
     setTimeout(async()=>{
         let url= baseUrl+"latitude="+currentLatitude+"&longitude="+currentLongitude+params;
-        let data=await getResponse(url);
-        let name =await getName(currentLatitude, currentLongitude);
-        console.log(name);
-        await populateData(data,name);
-        console.log(currentLatitude);
+        currentData=await getResponse(url);
+        currentName =await getName(currentLatitude, currentLongitude);
+        console.log(currentName);
+        await populateData();
+        await refreshDropdown();
+        console.log("Loaded Data");
     },1000);
 }
 
-async function populateData(data,name){
+async function populateData(){
+
+    //left side page
+    let name = currentName;
+    let data = currentData;
     let location=document.getElementById('loc')
     location.innerHTML=name;
     // document.append(location);
-    let temp=document.getElementById('temparatere');
+    let temp=document.getElementById('temperature');
     temp.innerHTML=data.current.temperature_2m+data.current_units.temperature_2m;
 
     let dateTimeStr = data.current.time+'Z';
@@ -121,16 +204,79 @@ async function populateData(data,name){
     revh.innerHTML = data.current.relative_humidity_2m +data.current_units.relative_humidity_2m;
     console.log(data);
 
+
+    // Right side page
+   await displayTable();
 }
 
-function updateDropdown(operation, val, name) {
-
-    if(operation === "ADD"){
-        refreshDropdown();
-    }else{
-        pinnedAreas.pop(val);
-//pop the area(val) from the areaList
+async function displayTable(){
+    let data = currentData;
+    let day_wise = document.getElementsByClassName('day-wise-info')[0];
+    let table;
+    if(document.getElementsByClassName('table_wise')[0]){
+         table = document.getElementsByClassName('table_wise')[0];
+        table.innerHTML = '';
+    }else {
+         table = document.createElement('table');
     }
+    table.border =1;
+    table.className='table_wise';
+    let header = table.createTHead();
+    let headerRow = header.insertRow(0);
+    let headers = ['Time', 'Temperature','Wind Speed', 'Humidity'];
+    headers.forEach(function(headerText) {
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode(headerText));
+        headerRow.appendChild(th);
+    });
+    let timelist = data.hourly.time;
+    let templist = data.hourly.relative_humidity_2m;
+    let windlist = data.hourly.temperature_2m;
+    let humidlist = data.hourly.wind_speed_10m;
+
+    for (let i = 0; i < 16; i++) {
+        let row = table.insertRow();
+        let cell1 = row.insertCell();
+        let cell2 = row.insertCell();
+        let cell3 = row.insertCell();
+        let cell4 = row.insertCell();
+
+        cell1.appendChild(document.createTextNode(await getDateTime(timelist[i]+'Z',filterValue)));
+        cell2.appendChild(document.createTextNode(templist[i]+data.hourly_units.relative_humidity_2m));
+        cell3.appendChild(document.createTextNode(windlist[i]+data.hourly_units.temperature_2m));
+        cell4.appendChild(document.createTextNode(humidlist[i]+data.hourly_units.wind_speed_10m));
+    }
+    day_wise.appendChild(table);
+}
+
+async function getDateTime(dateTimeStr,filterValue){
+    const timeZone = 'Asia/Kolkata';
+// Convert the date-time string to the desired time zone
+    let dateInTime = new Date(dateTimeStr).toLocaleString("en-US", { timeZone });
+    if(filterValue === 'hourly'){
+        return dateInTime.split(',')[1];
+    }
+    else{
+        return dateInTime;
+    }
+}
+
+async function PinOrUnpinDropDown(operation) {
+    //based on pinValue -> pin / unpin,
+    console.log(operation);
+    if(operation === "pin"){
+        let area = new Area();
+        let obj=await area.create(currentName, currentLatitude, currentLongitude);
+        pinnedAreas.push(currentName);
+        console.log(pinnedAreas);
+        areaList[currentName]=obj;
+        console.log(areaList);
+    }else if(operation === 'unpin'){
+        //remove the currentName and its area object from existing lists
+        pinnedAreas.pop(currentName);
+        delete areaList[currentName];
+    }
+    await refreshDropdown();
 }
 
 async function getName(latitude, longitude){
@@ -139,6 +285,7 @@ async function getName(latitude, longitude){
         const data = await response.json();
 
         const address = data.address;
+        currentName = address.city || address.town || address.village || 'Not Found';
         return address.city || address.town || address.village || 'City not found';
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -146,33 +293,28 @@ async function getName(latitude, longitude){
     }
 
 }
-function refreshDropdown(){
+async function refreshDropdown(){
+// Get the dropdown element
+    const dropdown = document.getElementById('location');
 
-    //add curentArea as an option child
-    //loop pinned areas as option childs
-    //if the currentarea is in pinned areas skip to add as an optionchild in dropdown
+    // Clear existing options in the dropdown
+    dropdown.innerHTML = '';
 
+    // Add currentArea as an option
+    const currentOption = document.createElement('option');
+    currentOption.value = currentName;
+    currentOption.textContent = currentName;
+    dropdown.appendChild(currentOption);
+    // Loop through pinned areas
+    pinnedAreas.forEach(area => {
+        if (area !== currentName) { // Skip if the area is the currentArea
+            const option = document.createElement('option');
+            option.value = area;
+            option.textContent = area;
+            dropdown.appendChild(option);
+        }
+    });
 }
-
-
-function savePinnedArea(){
-    let name= document.getElementById('location');
-    let obj=new Area(name, currentLatitude, currentLongitude);
-    pinnedAreas.push(currentArea);
-    areaList[currentArea]=obj;
-
-}
-
-
-
-
-function updateCurrentLocation(value){
-    currentArea=value;
-    currentName=document.getElementById('location');
-
-}
-
-
 
 async function getResponse(url){
     const response = await fetch(url,{method:'GET'});
@@ -198,10 +340,3 @@ async function getLocation(){
 }
 
 
-class Area {
-    constructor(name, lat, lng){
-        this.name= name;
-        this.lat= lat;
-        this. lng=lng;
-    }
-}
